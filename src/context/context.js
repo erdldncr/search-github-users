52c623185f352eae39c3d57601c56fc1929dcdf7
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-05-02 01:22:18 +0100

    first commit

---
+++ src/context/context.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import mockUser from './mockData.js/mockUser';
+import mockRepos from './mockData.js/mockRepos';
+import mockFollowers from './mockData.js/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
+
+export const GithubContext=React.createContext()
+
+export function GithubProvider({children}){
+
+return <GithubContext.Provider value={'dsds'}>
+    {children}
+
+</GithubContext.Provider>
+}
